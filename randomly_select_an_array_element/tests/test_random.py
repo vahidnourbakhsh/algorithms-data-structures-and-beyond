718676c17ac9ea64faaa5adf57c6823e78d0e7fb
Vahid Nourbakhsh <vnourba@gmail.com>
2022-02-02 09:02:26 -0800

    feat: add test

---
+++ randomly_select_an_array_element/tests/test_random.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+import math
+import random
+
+from randomly_select_an_array_element.random3 import RichArray
+
+def test_get_random():
+    random.seed(10)
+    array = RichArray([1, 2, 3, 4])
+    counts = defaultdict(int)
+    repetitions = 10000
+    for _ in range(repetitions):
+        e = array.get_random()
+        counts[e] += 1
+    actual_probabilities = {e: v / repetitions for e, v in counts.items()}
+    print(f"actual: {actual_probabilities} \n expected: {{ 1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4 }}")
+    assert math.isclose(actual_probabilities[1], 0.1, abs_tol=0.1)
+    assert math.isclose(actual_probabilities[2], 0.2, abs_tol=0.1)
+    assert math.isclose(actual_probabilities[3], 0.3, abs_tol=0.1)
+    assert math.isclose(actual_probabilities[4], 0.4, abs_tol=0.1)
